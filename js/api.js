7044acf004882858f7cf4150b3443b8b6030effb
Bergeron Mahelie <143533122+bermah1331144@users.noreply.github.com>
2025-02-22 13:33:53 -0500

    Etape 4 rendu a la boite dialogue

--- js/api.js
+++ js/api.js
@@ -72,3 +72,42 @@ async function insererDataPublications(dataPublications){
     })
 
 }
+
+// Fonciton pour ajouter les infromation des publications automatiquement pour les identifiant et les date
+
+//funtion qui va générer ID unique
+function generateId() {
+    let lastId = localStorage.getItem("lastPublicationId");
+    let newId = lastId ? parseInt(lastId) + 1 : 1;
+    localStorage.setItem("lastPublicationId", newId);
+    return newId;
+}
+
+//fonctiyon pour optenir la date actuelle
+function getCurrentDate() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;   
+}
+
+//fonction pour ajouter les information
+async function addInfomation(){
+    const publicationId = generateId();
+    const date = getCurrentDate();
+    const content = document.getElementById("content").value;
+    const dataPublications = { publicationId, date, content };
+    const response = await fetch("http://localhost:3000/publications", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataPublications)
+    });
+
+    if (response.ok) {
+            const data = await response.json();
+            createDataPublications(data);
+        }
+}
