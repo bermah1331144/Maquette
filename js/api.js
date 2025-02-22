c93a076055dc3b1d66f09ef18d54b434fe7318f7
Bergeron Mahelie <143533122+bermah1331144@users.noreply.github.com>
2025-02-22 12:47:22 -0500

    Avancer l'éatpe 4

--- js/api.js
+++ js/api.js
@@ -7,20 +7,29 @@ http://localhost:3000/publications
 http://localhost:3000/commentaires
 */ 
 
+
+/*ATTENTION !!!! Je n'Est pas pu vérifier certaines fonctionnalités du site
+    Je dois les tester en local
+    Voici la liste:
+        - add dans la fonction createDataPublications(${****})
+        -Tester fonction async function insererDataPublications(dataPublications) */
+        
+
+
 //Obtenir les publications
 async function getPublicationsAsyc(){
     try {
         const response = await fetch("http://localhost:3000/publications");
         const data = await response.json();
-        insertDataPublications(data);
+        createDataPublications(data);
     } catch (error) {
         console.log(error);
     }
 
 }
 
-//doit creer une fonction pour inserer des publications
-function insertDataPublications(data){
+//doit creer une fonction pour creer les publications dans les cards
+function createDataPublications(data){
     const dataPublications = document.getElementById("card-body");
     
     if(!dataPublications) return;
@@ -35,8 +44,8 @@ function insertDataPublications(data){
             <div class="card">
                 <img src="images/img-card-2.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text.</p>
+                <h5 class="card-title">${publication.titre}</h5>
+                    <p class="card-text">${publication.contenu}</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
@@ -45,6 +54,21 @@ function insertDataPublications(data){
 
         return dataPublications;
 
+    })
+
+}
+
+//Fontion pour inserer les informations des publications
+async function insererDataPublications(dataPublications){
+    const dataPublications = document.getElementById("card-body");
+    
+    if(!dataPublications) return;
+
+    dataPublications.innerHTML = "";  //vider le contenu
+
+    publications.forEach(publication => {
+        const card = document.createElement(createDataPublications(publication));
+        dataPublications.appendChild(card);
     })
 
 }
