d99aa078668ce1738f3df0550e608c164d5d8589
mahelie <mahelie.bergeron@gmail.com>
2025-02-23 12:58:42 -0500

    Rendu a etape 5 publication FAIT Mais doit corriger erreur/ Commentaire commencer a tester

--- js/api.js
+++ js/api.js
@@ -5,17 +5,15 @@ http://localhost:3000/
 EndPoints :
 http://localhost:3000/publications
 http://localhost:3000/commentaires
-*/ 
+*/
 
 
-/*ATTENTION !!!! Je n'Est pas pu vérifier certaines fonctionnalités du site
-    Je dois les tester en local
-    Voici la liste:
-        - add dans la fonction createDataPublications(${****})
-        -Tester fonction async function insererDataPublications(dataPublications) */
-        
 
+const { get } = require("jquery");
 
+/*--------------------  Requetes a porpos de PUBLICATIONS --------------------*/
+
+const dataPublications = document.getElementById("card-body");
 //Obtenir les publications
 async function getPublicationsAsyc(){
     try {
@@ -28,9 +26,11 @@ async function getPublicationsAsyc(){
 
 }
 
+
 //doit creer une fonction pour creer les publications dans les cards
+
 function createDataPublications(data){
-    const dataPublications = document.getElementById("card-body");
+    //const dataPublications = document.getElementById("card-body");
     
     if(!dataPublications) return;
 
@@ -58,10 +58,13 @@ function createDataPublications(data){
 
 }
 
+
+
+
 //Fontion pour inserer les informations des publications
+
 async function insererDataPublications(dataPublications){
-    const dataPublications = document.getElementById("card-body");
-    
+    const publications = await getPublicationsAsyc();
     if(!dataPublications) return;
 
     dataPublications.innerHTML = "";  //vider le contenu
@@ -73,15 +76,18 @@ async function insererDataPublications(dataPublications){
 
 }
 
+
 // Fonciton pour ajouter les infromation des publications automatiquement pour les identifiant et les date
 
 //funtion qui va générer ID unique
+
 function generateId() {
     let lastId = localStorage.getItem("lastPublicationId");
     let newId = lastId ? parseInt(lastId) + 1 : 1;
     localStorage.setItem("lastPublicationId", newId);
     return newId;
 }
+
 
 //fonctiyon pour optenir la date actuelle
 function getCurrentDate() {
@@ -92,7 +98,9 @@ function getCurrentDate() {
     return `${year}-${month}-${day}`;   
 }
 
+
 //fonction pour ajouter les information
+// ATTENTION !! ERREUR au niveau de la promesse !!!!!!!
 async function addInfomation(){
     const publicationId = generateId();
     const date = getCurrentDate();
@@ -111,3 +119,70 @@ async function addInfomation(){
             createDataPublications(data);
         }
 }
+
+
+/*--------------------  Requetes a porpos de COMMENTAIRES --------------------*/
+//Doit faire une requete AJAX a l'API pour obtenir la publication. 
+// en utilisante les query parameters je doit opteniridentifiant a charger
+
+//appeler la fonctione pour obternir les infomartion de la publication
+//!!!! Pas sur que j'en est BEOSIN ??
+async function getIdLoadAsyc(params) {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    const id = urlParams.get("id");
+    try {
+        const response = await fetch("http://localhost:3000/publications/" + id);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        createDataPublications(data);
+        insererDataPublications(data);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+
+//fonction obterni commentaires
+async function getCommentairesAsyc(){
+    try {
+        const response = await fetch("http://localhost:3000/commentaires");
+        const data = await response.json();
+        createDataCommentaires(data);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+//fonction creation des commentaires
+
+function createDataCommentaires(data){
+    const dataCommentaires = document.getElementById("commentaires");
+    dataCommentaires.innerHTML = "";
+
+    data.forEach(commentaire => {
+        dataCommentaires.innerHTML += `           
+            <div class="container commentaire">
+                <div class="d-flex justify-content-left">
+                    <svg class = "img-client" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-square" viewBox="0 0 25 25">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                    <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
+                    </svg>
+                    <p class="text-start">Start aligned text on all viewport sizes.</p>
+                </div>
+            </div>
+        `;
+        return dataCommentaires;
+    })
+}
+
+
+//consultation d'un commentaire
+async function getCommentAsyc(params) {
+    
+}
+//Ajouter un commmtaire 
---
+++ js/jquery-ui.js
@@ -0,0 +1,52 @@
+//PAGE FormulaireAdd
+    //Boite dialogue
+
+/*initalise laboite de dialogue
+$("#openDialog").click(function () {
+    $("#collapseForm").collapse("show");
+});
+
+//ferme la boite de dialogie
+$("#closeDialog").click(function () {
+    $("#collapseForm").collapse("hide");
+});
+
+//chagement de la boite dialogue
+$("#boiteDialogue").dialog({
+    autoOpen: false,
+    modal: true,
+    buttons: {
+
+        Oui: function () {
+            $(this).dialog("close");
+            window.location.href = "Page_Principale.html";
+        },
+        Non: function () {
+            $(this).dialog("close");
+        }
+    }
+});*/
+
+//Fonction qui vas enclancher la boite de dialogue
+//ATTENTION J,ai une erreur que je ne comprend pas 
+$(document) .ready(function () {
+    $("#openDialog").click(function () {
+        $("#collapseForm").collapse("show");
+    });
+    $("#boiteDialogue").dialog({
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            Oui: function () {
+                $(this).dialog("close");
+                window.location.href = "Page_Principale.html";
+            },
+            Non: function () {
+                $(this).dialog("close");
+            }
+        }
+    });
+    $("#closeDialog").click(function () {
+        $("#collapseForm").collapse("hide");
+    });
+})
