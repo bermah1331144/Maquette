062c316d7dbcee9aebc52c15dbd9da8ff52e0cfa
mahelie <mahelie.bergeron@gmail.com>
2025-03-03 13:44:14 -0500

    Mes pages principale et blog fonctionne dynamiquement. ON TOUCHE PLUS MAHÉLIE !!

--- js/api_PageBlog.js
+++ js/api_PageBlog.js
@@ -1,13 +1,13 @@
 /* ----------- Affichage de ma page blogue ----------- */
 
-document.addEventListener("DOMContentLoaded", getCommentairesAsyc);
+document.addEventListener("DOMContentLoaded",() => afficherCommentaireBlog(publicationId));
 
 //fonction recuperer les commentaires DEJA dans mon API
 async function getCommentairesAsyc(){
     try {
         const response = await fetch("http://localhost:3000/commentaires");
         const commentaire = await response.json();
-        affichereDataCommentaires(commentaire);
+        affichereDataCommentaires(publicationId,commentaire);
     } catch (error) {
         console.log(error);
     }
@@ -25,48 +25,7 @@ function recuperationIdentifiant(params) {
 
 const publicationId = recuperationIdentifiant();
 
-//fonction afficher des commentaires dans ma page blog
-function afficherCommentaireBlog(publicationId){
-    const dataCommentaires = document.getElementById("commentaire");
-    
-    if(!dataCommentaires) return;
 
-    dataCommentaires.innerHTML = "";
-
-    fetch("http://localhost:3000/publications/" + publicationId)
-        .then(response => response.json())
-        .then(commentaires => { 
-            if(!commentaires.length === 0){
-                dataCommentaires.innerHTML += "<p>Aucun commentaire</p>";
-                return
-                }
-
-            })
-
-            commentaires.forEach(commentaires => {
-                dataCommentaires.innerHTML += `           
-                    <h1 class="text-center" id="titre-jeu">${publications.titre}</h1>
-                    <div class="text-start" id="text-jeu">
-                        <p>
-                            ${commentaires.contenu}
-                        </p>
-                    </div>
-                    <div class="text-center col-12">
-                        <img src="/images/img-blog.jpg" class="img-fluid" alt="...">
-                        <p class="text-">Image</p>
-                    </div>
-                    <div class="text-start" id="text-jeu">
-                        <p>
-                            ${commentaires.description}
-                        </p>
-                    </div>
-                `;
-        })
-
-}
-
-
-document.addEventListener("DOMContentLoaded", afficherCommentaireBlog(publicationId));
 
 
 
@@ -89,20 +48,29 @@ function generateIdCommentraire() {
 
 
 //envoyer donnée commentaire au server Json et rempli les attributs id/date/contenu/description
-async function ajouterCommentaires(commentaire){
+async function ajouterCommentaires(){
+    const contenuCommentaire = $("#commentaire").val();
+    const descriptionCommentaire = $("#description").val();
+
+    if(!contenuCommentaire || !descriptionCommentaire){
+        alert("Veuillez remplir tous les champs");
+        return;
+    }   
+
+    const attributsCommentaire = {
+        id: generateIdCommentraire(),
+        publicationId: publicationId,
+        date: generationDateCommentaire(),
+        contenu: contenuCommentaire
+    };
+
     try {
         const response = await fetch("http://localhost:3000/commentaires", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({
-                id: $('#id').val(),
-                date: $('#date').val(),
-                contenu: $('#commentaire').val(),
-                description : $('#description').val()
-
-            })
+            body: JSON.stringify(attributsCommentaire)
         });
 
         if (!response.ok) {
@@ -112,18 +80,35 @@ async function ajouterCommentaires(commentaire){
         const resultat = await response.json();
         console.log("Votre commentaire a étét ajoutée avec succes :",resultat);
         document.getElementById("commentaire").value = "";
+        afficherCommentaireBlog(publicationId);
     } catch (error) {
         console.log(error);
     }
 }
 
-
 //declenchement de l'evenement
-document.getElementById("envoyer").addEventListener("click", ajouterCommetaires(commentaire));
-/* ----------Doit faire une requete jQuery -----------*/
-$(document).ready(function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const publicationId = urlParams.get("id");
-    getCommentairesAsyc(publicationId);
-    chargerCommentairesAfficher();
-})
+document.getElementById("envoyer").addEventListener("click", ajouterCommentaires);
+
+
+
+/*----------------------------Afficher les commentaires----------------------------*/
+function afficherCommentaireBlog(publicationId){
+    fetch(`http://localhost:3000/commentaires?publicationId=${publicationId}`)
+        .then(response => response.json())
+        .then(commentaires => {
+            const dataCommentaires = document.getElementById("commentaireBd");
+            dataCommentaires.innerHTML = "";
+            commentaires.forEach(commentaires => {
+                dataCommentaires.innerHTML += `
+                
+                <div class="d-flex justify-content-left">
+                    <svg class = "img-client" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-square" viewBox="0 0 25 25">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                    <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
+                    </svg>
+                    <p class="text-start">${commentaires.contenu}</p>
+                </div>
+                `;
+            });
+        });
+}           
