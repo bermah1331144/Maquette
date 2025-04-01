770b14b42ed695f031fcdd13c5ada7bfb0bc3c21
mahelie <mahelie.bergeron@gmail.com>
2025-04-01 11:15:30 -0400

    termimer Lab3

--- Composants/Comment.js
+++ Composants/Comment.js
@@ -1,4 +1,4 @@
-function Comment ({CommentaireID}){
+function Comment ({commentaire}){
     return (
     <div className="container" id="commentaireBd">
         <div className="d-flex justify-content-left">
@@ -6,7 +6,7 @@ function Comment ({CommentaireID}){
             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
             </svg>
-            <p className="text-start">Start aligned text on all viewport sizes.</p>
+            <p className="text-start">{commentaire.contenu}</p>
         </div>
     </div>
     )
